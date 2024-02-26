34440e2ab157cc5046195a42b50d40144ffafa84
xander <al.pavlichenko2014@gmail.com>
2024-02-26 23:45:38 +0300

    update 0.1, create project and write first screen

--- app/iqj/android/app/src/main/kotlin/com/studentsmirea/iqj/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.studentsmirea.iqj
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ app/iqj/lib/widgets/welcome.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class Welcome extends StatelessWidget {
+  const Welcome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Image(image: AssetImage('assets/images/welcome/welcome_1.png')),
+              SizedBox(
+                height: 90,
+              ),
+              Text(
+                'Добро\nпожаловать!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 36,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 210,
+              ),
+              FilledButton(
+                onPressed: null,
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStatePropertyAll<Color>(Color(0xFFEFAC00)),
+                  padding: MaterialStatePropertyAll(
+                    EdgeInsets.only(left: 110, right: 110, top: 20, bottom: 20),
+                  ),
+                ),
+                child: Text(
+                  'Далее',
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
