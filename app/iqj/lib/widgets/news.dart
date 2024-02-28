2bb89c70278a2d7db25039ce602a23a15236fd13
PRPDSS <prpdss@gmail.com>
2024-02-28 21:47:28 +0300

    modified news widget and android/ios manifests

--- app/iqj/lib/widgets/news.dart
+++ app/iqj/lib/widgets/news.dart
@@ -1,5 +1,10 @@
+// Основной виджет - News
+
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:http/http.dart' as http;
 
 class News extends StatefulWidget {
   const News({super.key});
@@ -9,19 +14,16 @@ class News extends StatefulWidget {
 }
 
 class _NewsState extends State<News> {
+  late Future<List<NewsArticle>> newsAlbum;
+
+  @override
+  void initState() {
+    super.initState();
+    newsAlbum = fetchNews();
+  }
 
   @override
   Widget build(BuildContext context) {
-    
-    //TODO: добавить макет карточки и генератор оной
-    final newsList = [
-      Placeholder(),
-      SizedBox(height: 9,),
-      Placeholder(),
-      SizedBox(height: 9,),
-      Placeholder(),
-    ];
-
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -55,12 +57,62 @@ class _NewsState extends State<News> {
         ],
       ),
 
-    body: ListView(
-      padding: const EdgeInsets.only(top: 8, left: 10, right: 10),
-      children: newsList,
-    ),
+      // TODO заменить на генератор блоков новостей
+      body: ListView(
+        padding: const EdgeInsets.only(left:12, right: 12),
+        children:[
+          Container(
+            color: Colors.black12,
+            margin: const EdgeInsets.only(top: 12),
+            height: 260,
+          ),
+          Container(
+            color: Colors.black12,
+            margin: const EdgeInsets.only(top: 12),
+            height: 260,
+          ),
+          Container(
+            color: Colors.black12,
+            margin: const EdgeInsets.only(top: 12),
+            height: 260,
+          ),
+          Container(
+            color: Colors.black12,
+            margin: const EdgeInsets.only(top: 12),
+            height: 260,
+          ),
+        ],
+      ),
     );
-
   }
+}
 
+Future<List<NewsArticle>> fetchNews() async {
+  // TODO Временно здесь будет пустышка, которую потом нужно заменить!!
+  final response = await http.get(Uri.parse('https://dummyjson.com/products'));
+  if (response.statusCode == 200){
+    return compute(parseArticles, response.body);
+  }
+  else {
+    throw Exception('Failed to load news');
+  }
+}
+
+List<NewsArticle> parseArticles(String body){
+  final parsed = (jsonDecode(body) as List).cast<Map<String, dynamic>>();
+
+  return parsed.map<NewsArticle>((json) => NewsArticle.fromJson(json)).toList();
+}
+
+class NewsArticle {
+  final String thumbnail;
+  final String title;
+  NewsArticle({required this.thumbnail, required this.title});
+
+  factory NewsArticle.fromJson(Map<String, dynamic> json) {
+    return NewsArticle(
+      thumbnail: json['image'] as String,
+      title: json['name'] as String,
+      );
+  }
 }
