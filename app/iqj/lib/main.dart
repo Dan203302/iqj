60efd013b788bc3527da5c0d30234d8816515c71
xander <al.pavlichenko2014@gmail.com>
2024-02-29 22:24:23 +0300

    update main

--- app/iqj/lib/main.dart
+++ app/iqj/lib/main.dart
@@ -1,33 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:iqj/widgets/welcome.dart';
-
-// void main() {
-//   const app = App();
-//   return runApp(app);
-// }
-
-// class App extends StatelessWidget {
-//   const App({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       theme: ThemeData(
-//         primaryColor: Colors.white,
-//         pageTransitionsTheme: const PageTransitionsTheme(
-//           builders: {
-//             TargetPlatform.android:
-//                 FadeUpwardsPageTransitionsBuilder(), // Анимация затухания для Android
-//             TargetPlatform.iOS:
-//                 FadeUpwardsPageTransitionsBuilder(), // Анимация затухания для iOS
-//           },
-//         ),
-//       ),
-//       home: const Welcome(),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:iqj/widgets/news.dart';
 import 'package:iqj/widgets/schedule.dart';
@@ -82,7 +52,7 @@ class _AppState extends State<App> {
             BottomNavigationBarItem(
               icon: Icon(Icons.grid_view),
               label: 'Сервисы',
-            ), // Добавлен новый элемент
+            ),
           ],
           currentIndex: _currentPage,
           fixedColor: const Color(0xFFEFAC00),
