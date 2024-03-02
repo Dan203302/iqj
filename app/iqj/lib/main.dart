9dbaccf8498d0805e58e0269e916c66bc61b8563
PRPDSS <prpdss@gmail.com>
2024-03-02 13:38:48 +0300

    Moved news related widgets to a separate folder

--- app/iqj/lib/main.dart
+++ app/iqj/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:iqj/widgets/news.dart';
+import 'package:iqj/widgets/news/news.dart';
 import 'package:iqj/widgets/schedule.dart';
 
 void main() => runApp(const App());
