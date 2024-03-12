ccb6bfe9fb368b93e30748cf8f09f6e1611be966
PRPDSS <prpdss@gmail.com>
2024-03-12 22:01:22 +0300

    basic welcome structure

---
+++ app/iqj/lib/features/welcome/data/list_of_panels.dart
@@ -0,0 +1,18 @@
+import 'package:iqj/features/welcome/domain/welcome_panel.dart';
+
+const List<WelcomePanel> welcomePanelList = [
+  WelcomePanel(
+    title: 'Добро пожаловать!',
+    image: 'assets/images/welcome_1.png',
+    imagePosition: 'top',
+  ),
+  WelcomePanel(
+    text: 'IQJ - инновационное приложение, разработанное специально для преподавателей университета МИРЭА',
+    image: 'assets/images/welcome_2.png',
+    imagePosition: 'bottom',
+  ),
+  WelcomePanel(
+    title: 'Бобро поржаловать!',
+    text: 'Тут вроде должен будет быть еще какой-то текст, но тексты придумывать не моя задча, моя задача - размещать их на вступительном экране!',
+  ),
+];
---
+++ app/iqj/lib/features/welcome/presentation/welcome.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+/*
+Добавить tabview со страницами из таблицы. Вероятно с генератором.
+*/
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
