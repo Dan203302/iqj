fced3e0d2802e21dad9249713179a7748673fe4a
Denis <ddm22026@gmail.com>
2024-03-15 21:33:03 +0300

    Первый образец чата

---
+++ chats/message.go
@@ -0,0 +1,10 @@
+package awesomeProject2
+
+type Message struct {
+	Author string `json:"author"`
+	Body   string `json:"body"`
+}
+
+func (self *Message) String() string {
+	return self.Author + " says " + self.Body
+}
