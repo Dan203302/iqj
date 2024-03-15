5cc7da7c8ebaf601a93d2cbaf2a157014b87fcb4
Tim <154249282+ptmff@users.noreply.github.com>
2024-03-16 00:13:17 +0300

    Update config.go to work properely on server

--- config/config.go
+++ config/config.go
@@ -2,4 +2,4 @@ package config
 
 // TODO: ФАЙЛ БЫЛ СОЗДАН КАК ПРИМЕР, ОБЯЗАТЕЛЬНО ДОБАВИТЬ В .gitignore ПЕРЕД ИЗМЕНЕНИЕМ!!!!!!!!!!!!
 
-var DbData = []interface{}{"hostname", "port", "user", "password", "dbname"}
+var DbData = []interface{}{"92.63.105.190", "5432", "postgres", "12345678", "postgres"}
