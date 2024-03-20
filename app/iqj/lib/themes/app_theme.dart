f5df773468968374ce98831b7905206d7f9c937f
PRPDSS <prpdss@gmail.com>
2024-03-21 01:43:51 +0300

    Modified color theme

---
+++ app/iqj/lib/themes/app_theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:iqj/themes/color_schemes.dart';
+import 'package:iqj/themes/text_theme.dart';
+
+ThemeData lightTheme = ThemeData(
+  colorScheme: lightColorScheme,
+  textTheme: textTheme,
+);
+
+ThemeData darkTheme = ThemeData(
+  colorScheme: darkColorScheme,
+  textTheme: textTheme,
+);
