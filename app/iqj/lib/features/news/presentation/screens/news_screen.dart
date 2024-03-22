3126e7f68be9db68757c036803b445335713a6aa
Denis <ddm22026@gmail.com>
2024-03-22 12:21:25 +0300

    Переход на большие новости

--- app/iqj/lib/features/news/presentation/screens/news_screen.dart
+++ app/iqj/lib/features/news/presentation/screens/news_screen.dart
@@ -12,6 +12,7 @@ import 'package:iqj/features/news/data/news_repository.dart';
 import 'package:iqj/features/news/presentation/bloc/news_bloc.dart';
 import 'package:iqj/features/old/news/newsListGenerator.dart';
 import 'package:iqj/features/old/schedule.dart';
+import 'package:iqj/main.dart';
 import 'package:iqj/theme/text_theme.dart';
 import 'package:shimmer/shimmer.dart';
 // import 'package:iqj/features/news/domain/news.dart';
@@ -148,41 +149,62 @@ class NewsCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 2,
-      margin: const EdgeInsets.all(8),
-      child: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image.network(thumbnail),
-            Row(
-              children: [
-                Expanded(
-                  child: Text(
-                    title,
-                    style: const TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      // title,
-                      // style: textTheme.titleLarge,
+    return InkWell(
+      onTap: () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const NewsList()), 
+      );
+    },
+      child: Card(
+        elevation: 2,
+        margin: const EdgeInsets.all(8),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.network(thumbnail),
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      title,
+                      style: const TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        // title,
+                        // style: textTheme.titleLarge,
+                      ),
                     ),
                   ),
-                ),
-                IconButton(
-                  onPressed: () {
-                    // Обработка нажатия кнопки
-                  },
-                  icon: SvgPicture.asset('assets/icons/news/bookmark.svg'),
-                ),
-              ],
-            ),
-            const SizedBox(height: 8),
-            Text(description),
-          ],
+                  IconButton(
+                    onPressed: () {
+                      // Обработка нажатия кнопки
+                    },
+                    icon: SvgPicture.asset('assets/icons/news/bookmark.svg'),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 8),
+              Text(description),
+            ],
+          ),
         ),
       ),
     );
   }
+}
+
+
+class NewsList extends StatelessWidget{
+  const NewsList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Здесь будет news"),),
+    );
+  }
+
 }
