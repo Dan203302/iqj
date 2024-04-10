8d9094a8520a62e434474f610cdcc5c6d539873b
PRPDSS <prpdss@gmail.com>
2024-04-10 22:06:10 +0300

    added status code check

--- app/iqj/lib/features/news/data/news_repository.dart
+++ app/iqj/lib/features/news/data/news_repository.dart
@@ -9,21 +9,25 @@ Future<List<News>> getNews() async {
       host: 'mireaiqj.ru',
       path: '/news',
       queryParameters: {'offset': '0', 'count': '15'},
+      //TODO сделать offset динамически изменяемым, чтоб получать следующие новости при страницы
     ),
   );
-  //TODO сделать offset динамически изменяемым, чтоб получать следующие новости при страницы
-  final List body =
-      (json.decode(response.body) as Map<String, dynamic>)['products'] as List;
-  return body.map(
-    (e) {
-      final json = e as Map<String, dynamic>;
-      return News(
-        id: json['id'] as String,
-        title: json['header'] as String,
-        publicationTime: DateTime.parse(['publication_time'] as String),
-        thumbnail: (json['image_link'] as List<String>)[0],
-        link: json['link'] as String,
-      );
-    },
-  ).toList();
+  if (response.statusCode == 200) {
+    final List body = (json.decode(response.body)
+        as Map<String, dynamic>)['products'] as List;
+    return body.map(
+      (e) {
+        final json = e as Map<String, dynamic>;
+        return News(
+          id: json['id'] as String,
+          title: json['header'] as String,
+          publicationTime: DateTime.parse(['publication_time'] as String),
+          thumbnail: (json['image_link'] as List<String>)[0],
+          link: json['link'] as String,
+        );
+      },
+    ).toList();
+  } else {
+    throw Exception(response.statusCode);
+  }
 }
