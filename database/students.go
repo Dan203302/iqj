0706374caa3ee14f7a7ea9ca61b8f7a8fc81986a
hnnsly <hynslei@ya.ru>
2024-04-10 22:20:47 +0300

    required functionality added

--- database/students.go
+++ database/students.go
@@ -11,19 +11,29 @@ func (st *Storage) createStudent(student *models.Student) error {
 	st.Mutex.Lock()
 	defer st.Mutex.Unlock()
 
-	jsonteachers, err := json.Marshal(student.Teachers)
+	jsonTeachers, err := json.Marshal(student.Teachers)
 	if err != nil {
 		return err
 	}
 
 	_, err = st.Db.Exec("INSERT INTO students (id,student_group,teachers) values ($1,$2,$3)",
-		student.Id, student.Group, jsonteachers)
+		student.Id, student.Group, jsonTeachers)
 
 	return err
 }
 
-// TODO: Сделать эту штуку но я не знаю есть ли смысл
-func (st *Storage) addTeachers(ids []int) error {
-	// ...
-	return nil
+func (st *Storage) addTeachers(student *models.Student, teachers *[]models.Teacher) error {
+	st.Mutex.Lock()
+	defer st.Mutex.Unlock()
+
+	jsonTeachers, err := json.Marshal(teachers)
+	if err != nil {
+		return err
+	}
+
+	_, err = st.Db.Exec("INSERT INTO students (teachers) values ($1)",
+		student.Id, student.Group, jsonTeachers)
+
+	return err
+
 }
