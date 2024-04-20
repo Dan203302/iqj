959e711d6d973051773982c5a0b9dea59aa66c27
daniil <hynslei@ya.ru>
2024-04-20 20:28:25 +0300

    classes struct init

---
+++ databaserework/classes.go
@@ -0,0 +1,13 @@
+package databaserework
+
+type Class struct {
+	Id       int
+	Groups   []int
+	Teacher  int
+	Count    int
+	Weekday  int
+	Week     int
+	Name     string
+	Type     string
+	Location string
+}
