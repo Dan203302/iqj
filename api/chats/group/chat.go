b1a633d78fa940472404dfa13d1b64d6de20e14e
Denis <ddm22026@gmail.com>
2024-04-23 21:35:52 +0300

    change package

--- api/chats/group/chat.go
+++ api/chats/group/chat.go
@@ -1,4 +1,4 @@
-package main
+package group
 
 import (
 	"database/sql"
@@ -85,3 +85,65 @@ func main() {
 		log.Fatal("Server startup failed:", err)
 	}
 }
+
+// package group
+
+// import (
+// 	"fmt"
+// 	"io"
+// 	"net/http"
+
+// 	//"github.com/gorilla/websocket"
+// 	"golang.org/x/net/websocket"
+// )
+
+// type Server struct {
+// 	conns map[*websocket.Conn]bool
+// }
+
+// func NewServer() *Server {
+// 	return &Server{
+// 		conns: make(map[*websocket.Conn]bool),
+// 	}
+// }
+
+// func (s *Server) handlWS(ws *websocket.Conn) {
+// 	fmt.Println("new connecting", ws.RemoteAddr())
+// 	s.conns[ws] = true
+
+// 	s.readLoop(ws)
+// }
+
+// func (s *Server) readLoop(ws *websocket.Conn) {
+// 	buf := make([]byte, 1024)
+// 	for {
+// 		n, err := ws.Read(buf)
+// 		if err != nil {
+// 			if err == io.EOF {
+// 				break
+// 			}
+// 			fmt.Println("read error", err)
+// 			continue
+// 		}
+// 		msg := buf[:n]
+// 		// fmt.Println(string(msg))
+// 		// ws.Write([]byte("u writed a msg"))
+// 		s.broadcast(msg)
+// 	}
+// }
+
+// func (s *Server) broadcast(b []byte) {
+// 	for ws := range s.conns {
+// 		go func(ws *websocket.Conn) {
+// 			if _, err := ws.Write(b); err != nil {
+// 				fmt.Println("write error", err)
+// 			}
+// 		}(ws)
+// 	}
+// }
+
+// func main() {
+// 	server := NewServer()
+// 	http.Handle("/ws", websocket.Handler(server.handlWS))
+// 	http.ListenAndServe(":8080", nil)
+// }
