470b2d5ec657ebebeb12731c7dd20c42a26dc753
stanlapru <stanlap@vk.com>
2024-04-25 00:13:29 +0300

    Fix listview in services

--- app/iqj/lib/features/services/presentation/screens/about_screen.dart
+++ app/iqj/lib/features/services/presentation/screens/about_screen.dart
@@ -57,140 +57,139 @@ class _AboutBloc extends State<AboutScreen> {
       ),
       body: Container(
         margin: EdgeInsets.only(left: 12, right: 12),
-        child: Expanded(
-          child: ListView(
-            children: <Widget>[
+        child: ListView(
+        children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _buildThumbnailImage(),
+              const Padding(padding: EdgeInsets.only(bottom: 12)),
+              Text(
+                'IQJ MIREA',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontFamily: 'Inter',
+                  fontSize: 32,
+                  color: Theme.of(context).colorScheme.onBackground,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              Text(
+                'Разработано с любовью студентами кафедры индустриального программирования ИПТИП для преподавателей <3',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontFamily: 'Inter',
+                  fontSize: 18,
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+              ),
+              const Padding(padding: EdgeInsets.only(bottom: 20)),
+              Text(
+                'Разработчики',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontFamily: 'Inter',
+                  fontSize: 32,
+                  color: Theme.of(context).colorScheme.onBackground,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  _buildThumbnailImage(),
-                  const Padding(padding: EdgeInsets.only(bottom: 12)),
                   Text(
-                    'IQJ MIREA',
-                    textAlign: TextAlign.left,
+                    'Тимлиды',
                     style: TextStyle(
                       fontFamily: 'Inter',
-                      fontSize: 32,
-                      color: Theme.of(context).colorScheme.onBackground,
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                  Text(
-                    'Разработано с любовью студентами кафедры индустриального программирования ИПТИП для преподавателей <3',
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                      fontFamily: 'Inter',
-                      fontSize: 18,
+                      fontSize: 24,
                       color: Theme.of(context).colorScheme.onBackground,
                     ),
                   ),
-                  const Padding(padding: EdgeInsets.only(bottom: 20)),
-                  Text(
-                    'Разработчики',
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                      fontFamily: 'Inter',
-                      fontSize: 32,
-                      color: Theme.of(context).colorScheme.onBackground,
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  Row(
                     children: [
-                      Text(
-                        'Тимлиды',
-                        style: TextStyle(
-                          fontFamily: 'Inter',
-                          fontSize: 24,
-                          color: Theme.of(context).colorScheme.onBackground,
-                        ),
-                      ),
-                      Row(
-                        children: [
-                          Text('Александр Павличенко - тимлид тимлидов'),
-                        ],
-                      ),
-                      Text('Лапутин Станислав - дизайн'),
-                      Text('Постников Даниил - бекенд API'),
-                      Text('Чуев Даниил - бекенд DB'),
-                      Text('Погосов Вячеслав - тестирование и QA'),
-                      const Padding(padding: EdgeInsets.only(bottom: 12)),
-                      Text(
-                        'Фронтенд',
-                        style: TextStyle(
-                          fontFamily: 'Inter',
-                          fontSize: 24,
-                          color: Theme.of(context).colorScheme.onBackground,
-                        ),
-                      ),
-                      Text('Александр Павличенко - тимлид'),
-                      Text('Лапутин Станислав'),
-                      Text('Дмитрив Денис'),
-                      Text('Баранова Софья'),
-                      Text('Кушаева Варвара'),
-                      Text('Хмара Екатерина'),
-                      Text('Богданов Максим'),
-                      const Padding(padding: EdgeInsets.only(bottom: 12)),
-                      Text(
-                        'Бекенд',
-                        style: TextStyle(
-                          fontFamily: 'Inter',
-                          fontSize: 24,
-                          color: Theme.of(context).colorScheme.onBackground,
-                        ),
-                      ),
-                      Text('Постников Даниил - тимлид API'),
-                      Text('Павленко Тимофей - API'),
-                      Text('Дмитрив Денис - API'),
-                      Text('Муртазин Руслан - API'),
-                      Text('Ермолина Софья - API'),
-                      Text('Чуев Даниил - тимлид DB'),
-                      Text('Романов Максим - DB'),
-                      Text('Игаев Даниил - DB'),
-                      Text('Бондаренко Фёдор - DB'),
-                      Text('Петрова Ксения - DB'),
-                      const Padding(padding: EdgeInsets.only(bottom: 12)),
-                      Text(
-                        'Тестировщики и QA',
-                        style: TextStyle(
-                          fontFamily: 'Inter',
-                          fontSize: 24,
-                          color: Theme.of(context).colorScheme.onBackground,
-                        ),
-                      ),
-                      Text('Погосов Вячеслав - тимлид тестировщиков и QA'),
-                      const Padding(padding: EdgeInsets.only(bottom: 12)),
-                      Text(
-                        'Дизайн приложения',
-                        style: TextStyle(
-                          fontFamily: 'Inter',
-                          fontSize: 24,
-                          color: Theme.of(context).colorScheme.onBackground,
-                        ),
-                      ),
-                      Text('Лапутин Станислав'),
-                      Text('Александр Павличенко'),
-                      const Padding(padding: EdgeInsets.only(bottom: 52)),
-                      Text(
-                        'Подробная информация',
-                        style: TextStyle(
-                          fontFamily: 'Inter',
-                          fontSize: 24,
-                          color: Theme.of(context).colorScheme.onBackground,
-                        ),
-                      ),
-                      Text('IQJ MIREA версия 0.0.1\nсборка 221803-24042024\nИПТИП/КИП (с) 2024 - Все права защищены.'),
-                      const Padding(padding: EdgeInsets.only(bottom: 512)),
-                      Text('Это место для пасхалок.'),
+                      Text('Александр Павличенко - тимлид тимлидов'),
                     ],
                   ),
+                  Text('Лапутин Станислав - дизайн'),
+                  Text('Постников Даниил - бекенд API'),
+                  Text('Чуев Даниил - бекенд DB'),
+                  Text('Погосов Вячеслав - тестирование и QA'),
+                  const Padding(padding: EdgeInsets.only(bottom: 12)),
+                  Text(
+                    'Фронтенд',
+                    style: TextStyle(
+                      fontFamily: 'Inter',
+                      fontSize: 24,
+                      color: Theme.of(context).colorScheme.onBackground,
+                    ),
+                  ),
+                  Text('Александр Павличенко - тимлид'),
+                  Text('Лапутин Станислав'),
+                  Text('Дмитрив Денис'),
+                  Text('Баранова Софья'),
+                  Text('Кушаева Варвара'),
+                  Text('Хмара Екатерина'),
+                  Text('Богданов Максим'),
+                  const Padding(padding: EdgeInsets.only(bottom: 12)),
+                  Text(
+                    'Бекенд',
+                    style: TextStyle(
+                      fontFamily: 'Inter',
+                      fontSize: 24,
+                      color: Theme.of(context).colorScheme.onBackground,
+                    ),
+                  ),
+                  Text('Постников Даниил - тимлид API'),
+                  Text('Павленко Тимофей - API'),
+                  Text('Дмитрив Денис - API'),
+                  Text('Муртазин Руслан - API'),
+                  Text('Ермолина Софья - API'),
+                  Text('Чуев Даниил - тимлид DB'),
+                  Text('Романов Максим - DB'),
+                  Text('Игаев Даниил - DB'),
+                  Text('Бондаренко Фёдор - DB'),
+                  Text('Петрова Ксения - DB'),
+                  const Padding(padding: EdgeInsets.only(bottom: 12)),
+                  Text(
+                    'Тестировщики и QA',
+                    style: TextStyle(
+                      fontFamily: 'Inter',
+                      fontSize: 24,
+                      color: Theme.of(context).colorScheme.onBackground,
+                    ),
+                  ),
+                  Text('Погосов Вячеслав - тимлид тестировщиков и QA'),
+                  const Padding(padding: EdgeInsets.only(bottom: 12)),
+                  Text(
+                    'Дизайн приложения',
+                    style: TextStyle(
+                      fontFamily: 'Inter',
+                      fontSize: 24,
+                      color: Theme.of(context).colorScheme.onBackground,
+                    ),
+                  ),
+                  Text('Лапутин Станислав'),
+                  Text('Александр Павличенко'),
+                  const Padding(padding: EdgeInsets.only(bottom: 52)),
+                  Text(
+                    'Подробная информация',
+                    style: TextStyle(
+                      fontFamily: 'Inter',
+                      fontSize: 24,
+                      color: Theme.of(context).colorScheme.onBackground,
+                    ),
+                  ),
+                  Text(
+                      'IQJ MIREA версия 0.0.1\nсборка 221803-24042024\nИПТИП/КИП (с) 2024 - Все права защищены.'),
+                  const Padding(padding: EdgeInsets.only(bottom: 512)),
+                  Text('Это место для пасхалок.'),
                 ],
               ),
-              const Padding(padding: EdgeInsets.only(bottom: 12)),
             ],
           ),
-        ),
+          const Padding(padding: EdgeInsets.only(bottom: 12)),
+        ],
+      ),
       ),
     );
   }
