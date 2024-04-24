aac736bff11e3613a37253b1935441397d234f32
Gewinn2 <daniilpostnickov@yandex.ru>
2024-04-24 21:59:13 +0300

    Added HandleSignUp error output to the console

--- api/handler/sign-up.go
+++ api/handler/sign-up.go
@@ -9,31 +9,51 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+// Регистрация
+// получает JSON в теле запроса вида:
+//
+//	{
+//		"email": "ivanovivan@yandex.ru",
+//		"password": "qwerty1234"
+//	}
+//
+// создает в бд пользователя с переданными данными.
+// POST /sign-up
 func (h *Handler) HandleSignUp(c *gin.Context) {
 	var user database.User
 
 	ok := c.BindJSON(&user)
 	if ok != nil {
-		c.String(http.StatusBadRequest, ok.Error())
+		c.JSON(http.StatusBadRequest, ok.Error())
+		fmt.Println("HandleSignUp:", ok)
+		return
 	}
 
 	if user.Email == "" {
 		c.JSON(http.StatusBadRequest, "There is no email")
+		fmt.Println("HandleSignUp: There is no email")
+		return
 	}
 
 	if user.Password == "" {
 		c.JSON(http.StatusBadRequest, "There is no password")
+		fmt.Println("HandleSignUp: There is no password")
+		return
 	}
 
 	password := user.Password
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
-		c.String(http.StatusBadRequest, err.Error())
+		c.JSON(http.StatusBadRequest, err.Error())
+		fmt.Println("HandleSignUp:", err)
+		return
 	}
 
 	user.Password = string(hashedPassword)
 	if err = database.Database.User.Add(&user); err != nil {
-		fmt.Println(err)
+		c.JSON(http.StatusInternalServerError, err.Error())
+		fmt.Println("HandleSignUp:", err)
+		return
 	}
 
 	c.JSON(http.StatusOK, user)
