5bde5ea09293884353ce507ed16648aac8ee6fa4
Gewinn2 <daniilpostnickov@yandex.ru>
2024-04-24 19:30:25 +0300

    Added a description of HandleAddNews and error output to the console

--- api/handler/news.go
+++ api/handler/news.go
@@ -20,34 +20,42 @@ func (h *Handler) HandleGetNews(c *gin.Context) {
 	offset, err := strconv.Atoi(offsetStr)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, err.Error())
+		fmt.Println("HandleGetNews:", err)
 		return
 	}
 	switch {
 	case offset < 0:
 		c.JSON(http.StatusBadRequest, "Offset < 0")
+		fmt.Println("HandleGetNews: offset < 0")
 		return
 	case offset > 999999:
 		c.JSON(http.StatusBadRequest, "Offset > 999999")
+		fmt.Println("HandleGetNews: offset > 999999")
 		return
 	}
 
 	count, err := strconv.Atoi(countStr)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, err.Error())
+		fmt.Println("HandleGetNews:", err)
 		return
 	}
 	switch {
 	case count < 1:
 		c.JSON(http.StatusBadRequest, "Count < 1")
+		fmt.Println("HandleGetNews: count < 1")
 		return
 	case count > 999999:
 		c.JSON(http.StatusBadRequest, "Count > 999999")
+		fmt.Println("HandleGetNews: count > 999999")
 		return
 	}
 
 	latestnews, err := database.Database.News.GetLatestBlocks(count, offset)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, "")
+		fmt.Println("HandleGetNews:", err)
+		return
 	}
 	c.JSON(http.StatusOK, latestnews)
 }
@@ -62,13 +70,17 @@ func (h *Handler) HandleGetNewsById(c *gin.Context) {
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
 		c.String(http.StatusBadRequest, err.Error())
+		fmt.Println("HandleGetNewsById:", err)
+		return
 	}
 	switch {
 	case id < 0:
 		c.JSON(http.StatusBadRequest, "Id < 0")
+		fmt.Println("HandleGetNewsById: id < 0")
 		return
 	case id > 999999:
 		c.JSON(http.StatusBadRequest, "Id > 999999")
+		fmt.Println("HandleGetNewsById: id > 999999")
 		return
 	}
 
@@ -77,15 +89,36 @@ func (h *Handler) HandleGetNewsById(c *gin.Context) {
 
 	news, err := database.Database.News.GetById(&newsDB)
 	if err != nil {
-		fmt.Println(err)
+		c.JSON(http.StatusInternalServerError, "")
+		fmt.Println("HandleGetNewsById:", err)
+		return
 	}
 	c.JSON(http.StatusOK, news)
 }
 
+// Для пользователей с ролью moderator
+// получает JSON в теле запроса вида:
+//
+//	{
+//		"header": " ",
+//		"link": " ",
+//		"image_link": [
+//			" "
+//		],
+//		"tags": [
+//			" "
+//		],
+//		"publication_time": " ",
+//		"text": " "
+//	}
+//
+// создает в бд переданную новость.
+// POST /api/news
 func (h *Handler) HandleAddNews(c *gin.Context) {
 	userIdToConv, exists := c.Get("userId")
 	if !exists {
 		c.JSON(http.StatusUnauthorized, "User ID not found")
+		fmt.Println("HandleAddNews:", exists)
 		return
 	}
 	userId := userIdToConv.(int)
@@ -94,16 +127,22 @@ func (h *Handler) HandleAddNews(c *gin.Context) {
 	user, err := database.Database.UserData.GetRoleById(&userDB)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, "")
+		fmt.Println("HandleAddNews:", err)
+		return
 	}
 	if user.Role == "moderator" {
 		var news database.News
 		err := c.BindJSON(&news)
 		if err != nil {
 			c.JSON(http.StatusBadRequest, err.Error())
+			fmt.Println("HandleAddNews:", err)
+			return
 		}
 		ok := database.Database.News.Add(&news)
 		if ok != nil {
 			c.JSON(http.StatusInternalServerError, ok.Error())
+			fmt.Println("HandleAddNews:", ok)
+			return
 		}
 		c.JSON(http.StatusOK, news)
 	} else {
