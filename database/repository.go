de9be1d1cbf43f2ea2d5e5171f9e23c8ae3de3e5
Gewinn2 <daniilpostnickov@yandex.ru>
2024-04-24 22:07:01 +0300

    Added methods for future implementation of clean architecture

--- database/repository.go
+++ database/repository.go
@@ -1,13 +1,23 @@
 package database
 
 type Authorization interface {
-	Add(u User) error
+	Add(u *User) error
+	GetById(u *User) (*User, error)
+	Check(u *User) (*User, error)
+	Delete(u *User) error
 }
 
 type NewsRepo interface {
+	Add(u *News) error
+	GetById(u *News) (*News, error)
+	Check(u *News) (*News, error)
+	Delete(u *News) error
 }
 
 type AdvertisementRepo interface {
+	Add(u *Advertisement) error
+	GetById(u *Advertisement) (*Advertisement, error)
+	Delete(u *Advertisement) error
 }
 
 type Schedule interface {
@@ -21,5 +31,10 @@ type Repository struct {
 }
 
 func NewRepository() *Repository {
-	return &Repository{}
+	return &Repository{
+		//Authorization: NewAuthPostgres(),
+		//NewsRepo: ,
+		//AdvertisementRepo: ,
+		//Schedule: ,
+	}
 }
--- service/service.go
+++ service/service.go
@@ -3,13 +3,23 @@ package service
 import "iqj/database"
 
 type Authorization interface {
-	Add(u database.User) error
+	Add(u *database.User) error
+	GetById(u *database.User) (*database.User, error)
+	Check(u *database.User) (*database.User, error)
+	Delete(u *database.User) error
 }
 
 type News interface {
+	Add(u *database.News) error
+	GetById(u *database.News) (*database.News, error)
+	Check(u *database.News) (*database.News, error)
+	Delete(u *database.News) error
 }
 
 type Advertisement interface {
+	Add(u *database.Advertisement) error
+	GetById(u *database.Advertisement) (*database.Advertisement, error)
+	Delete(u *database.Advertisement) error
 }
 
 type Schedule interface {
@@ -23,5 +33,10 @@ type Service struct {
 }
 
 func NewService(repository *database.Repository) *Service {
-	return &Service{}
+	return &Service{
+		Authorization: NewAuthService(repository.Authorization),
+		//News: ,
+		//Advertisement: ,
+		//Schedule:
+	}
 }
