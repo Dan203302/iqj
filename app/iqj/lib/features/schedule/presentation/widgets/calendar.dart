a2917b1470245f9e899b59434f93a68379235da0
PRPDSS <prpdss@gmail.com>
2024-04-25 16:34:18 +0300

    Исправлена ошибка в расписании

--- app/iqj/lib/features/schedule/presentation/widgets/calendar.dart
+++ app/iqj/lib/features/schedule/presentation/widgets/calendar.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-
 class Calendar extends StatefulWidget {
   const Calendar({super.key});
 
@@ -15,75 +14,70 @@ class _CalendarState extends State<Calendar> {
   DateTime focusedDay = DateTime.now();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: content(),
-    );
+    return content();
+    //   Scaffold(
+    //   appBar: AppBar(),
+    //   body: content(),
+    // );
   }
-  Widget content(){
-    return Column(
-      children: [
-        Container(
-          // создания календаря
-          child: TableCalendar(
-            //locale: "rus",
-            rowHeight: 47,
-            availableGestures: AvailableGestures.all,
-            focusedDay: selectedDay,
-            firstDay: DateTime(2023, 1, 1),
-            lastDay: DateTime(2030, 1, 1),
-            calendarFormat: format,
-            onFormatChanged: (CalendarFormat _format) {
-              setState(() {
-                format = _format;
-              });
-            },
-            startingDayOfWeek: StartingDayOfWeek.monday,
-            daysOfWeekVisible: true,
-            // изменение дня 
-            onDaySelected: (DateTime selectDay, DateTime focusDay) {
-              setState(() {
-                selectedDay = selectDay;
-                focusedDay = focusDay;
-              });
-              print(focusedDay);
-            },
-            selectedDayPredicate: (DateTime date){
-              return isSameDay(selectedDay, date);
-            },
-            // внешний вид календаря
-            calendarStyle: const CalendarStyle(
-              isTodayHighlighted: true,
-              selectedDecoration: BoxDecoration(
-                color: Color(0xFFC48F05),
-                shape: BoxShape.circle,
-              ),
-              selectedTextStyle: TextStyle(color: Colors.white),
-              todayDecoration: BoxDecoration(
-                color: const Color(0xFF454648),
-                shape: BoxShape.circle,
-              ),
-              defaultDecoration: BoxDecoration(
-                shape: BoxShape.circle,
-              ),
-              weekendDecoration: BoxDecoration(
-                shape: BoxShape.circle,
-              ),
-            ),
-            headerStyle: HeaderStyle(
-              formatButtonVisible: true,
-              formatButtonShowsNext: false,
-              formatButtonDecoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.background,
-                borderRadius: BorderRadius.circular(5.0),
-              ),
-              formatButtonTextStyle: const TextStyle(
-                color: Colors.white,
-              ),
-            ),
-          ),
+
+  Widget content() {
+    return TableCalendar(
+      //locale: "rus",
+      rowHeight: 47,
+      availableGestures: AvailableGestures.all,
+      focusedDay: selectedDay,
+      firstDay: DateTime(2023, 1, 1),
+      lastDay: DateTime(2030, 1, 1),
+      calendarFormat: format,
+      onFormatChanged: (CalendarFormat _format) {
+        setState(() {
+          format = _format;
+        });
+      },
+      startingDayOfWeek: StartingDayOfWeek.monday,
+      daysOfWeekVisible: true,
+      // изменение дня
+      onDaySelected: (DateTime selectDay, DateTime focusDay) {
+        setState(() {
+          selectedDay = selectDay;
+          focusedDay = focusDay;
+        });
+        print(focusedDay);
+      },
+      selectedDayPredicate: (DateTime date) {
+        return isSameDay(selectedDay, date);
+      },
+      // внешний вид календаря
+      calendarStyle: const CalendarStyle(
+        isTodayHighlighted: true,
+        selectedDecoration: BoxDecoration(
+          color: Color(0xFFC48F05),
+          shape: BoxShape.circle,
         ),
-      ],
+        selectedTextStyle: TextStyle(color: Colors.white),
+        todayDecoration: BoxDecoration(
+          color: const Color(0xFF454648),
+          shape: BoxShape.circle,
+        ),
+        defaultDecoration: BoxDecoration(
+          shape: BoxShape.circle,
+        ),
+        weekendDecoration: BoxDecoration(
+          shape: BoxShape.circle,
+        ),
+      ),
+      headerStyle: HeaderStyle(
+        formatButtonVisible: true,
+        formatButtonShowsNext: false,
+        formatButtonDecoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.background,
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        formatButtonTextStyle: const TextStyle(
+          color: Colors.white,
+        ),
+      ),
     );
   }
 }
