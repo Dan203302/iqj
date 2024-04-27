473087b1d4e8da49e2ab5691172ce9c67269860d
Gewinn2 <daniilpostnickov@yandex.ru>
2024-04-27 09:47:20 +0300

    .

---
+++ api/handler/handler.go
@@ -0,0 +1,53 @@
+package handler
+
+import (
+	"github.com/mirea/iqj/iqj/api/excelparser"
+	"iqj/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Получает criterion (group, tutor, classroom) и value из запроса, вызывает функцию Parse,
+// которая вернет массив с расписанием по заданным критериям.
+// Выдает расписание пользователю в формате JSON.
+// Например при GET /lessons?criterion=group&value=ЭФБО-01-23 вернет расписание группы ЭФБО-01-23.
+// При неверном критерии или значении отправит null
+
+func Lessons(c *gin.Context) {
+	criterion := c.Query("criterion")
+	value := c.Query("value")
+
+	lessons, err := excelparser.Parse(criterion, value)
+	if err != nil {
+		c.String(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	var filteredLessons []models.Lesson
+	for _, lesson := range lessons {
+		switch criterion {
+		case "group":
+			for _, group := range lesson.GroupID {
+				if group == 0 { //ЗАМЕНИТЬ НА  group == value
+					filteredLessons = append(filteredLessons, lesson)
+				}
+			}
+
+		case "tutor":
+			if lesson.TeacherID == 0 { //ЗАМЕНИТЬ НА lesson.TeacherID == value
+				filteredLessons = append(filteredLessons, lesson)
+			}
+
+		case "classroom":
+			if lesson.Location == value {
+				filteredLessons = append(filteredLessons, lesson)
+			}
+		default:
+			c.String(http.StatusBadRequest, err.Error())
+			return
+		}
+	}
+
+	c.JSON(http.StatusOK, filteredLessons)
+}
